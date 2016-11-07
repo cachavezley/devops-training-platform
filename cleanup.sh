67a75924685dda10a8c3a546c1d3a901348b0e0b
Christian Alonso Chavez Ley <cachavezley@sqli.com>
2016-11-07 19:47:59 +0100

    - show the total elapsed time for both scripts - added some comments

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+START_TIME=$SECONDS
+
+echo "INFO: Destroying Docker Containers"
 docker-compose down
 
+echo "INFO: Cleaning up Jenkins Data"
 sudo chown -R $(whoami):$(whoami) data/
 rm -rf data/*
 rm -rf $(echo data/.[^.]*)
 
 rm -f jenkins-cli.jar
 
+echo "INFO: Destroying Vagrant Box"
 vagrant destroy -f
+
+echo "INFO: Removing Jenkins Docker Image"
 docker rmi training/jenkins
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "TOTAL CLEANUP TIME: $(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
--- setup.sh
+++ setup.sh
@@ -17,11 +17,22 @@ function declareNode() {
 }
 
 ###########################		MAIN SCRIPT         ###########################
+START_TIME=$SECONDS
+
+echo "INFO: Creating Vagrant Box"
 vagrant up
+
+echo "INFO: Building Jenkins Docker Image"
 docker build -t training/jenkins .
 docker-compose up -d
 
+echo "INFO: Waiting for Jenkins to start..."
 sleep 20
+
+echo "INFO: Setting up Jenkins"
 downloadJenkinsClient
 declareCredentials
 declareNode
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "TOTAL SETUP TIME: $(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
